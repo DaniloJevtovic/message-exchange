d9b12117e7dd00a0100a3d204e06aeb39c6bb0be
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2023-03-02 14:09:25 +0100

    inicijalni projekat

---
+++ src/main/java/com/messages/messageexchange/MessageExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.messages.messageexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/messages/messageexchange/MessageExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messages.messageexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
